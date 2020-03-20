0b5a9d436b2a02135547283ef479e693393b00b7
JacksonKjar <61400994+JacksonKjar@users.noreply.github.com>
2020-03-19 20:11:42 -0500

    Fixed bugs & format
    Formatting changes and fixed an issue when multiple definitions are on same page

--- __init__.py
+++ __init__.py
@@ -16,23 +16,20 @@ class dictionaryEntry:
     @classmethod
     def fromSearchPage(cls, name, dataHTML):
         shortDef = re.search(r'<p class="text">(.+?)</p>',dataHTML).group(1)
-        url = "https://dictionary.goo.ne.jp"+re.search(r'/word/.+?"',dataHTML).group(0)
+        url = "https://dictionary.goo.ne.jp"+re.search(r'(/word/.+?)"',dataHTML).group(1)
         word = re.search(r'<p class="title">(.+?) ',dataHTML).group(1)
         return cls(name, word, shortDef, url)
 
     @classmethod
     def fromEntryPage(cls, name, dataHTML):
-        return cls(name,
-            re.search(r'"og:title" content="(.+?)の意味',dataHTML,re.DOTALL).group(1),
-            re.search(r'"description" content=".+?類語。(.+?)- goo国語',dataHTML,re.DOTALL).group(1),
-            "")
+        word = re.search(r'"og:title" content="(.+?)の意味',dataHTML,re.DOTALL).group(1)
+        reg = re.compile('<div id="jn-.+?_".+?<div class="content-box contents_area meaning_area p10">(.+?)<!-- /contents -->',re.DOTALL)
+        shortDef = cleanDefinition(re.search(reg,dataHTML).group(1))
+        return cls(name, word, shortDef, "")
 
     def __init__(self, name, word, shortDef, url):
-        while("<img" in shortDef):
-            matches = re.search(r'(.*)<img.+?>(.*)',shortDef,re.DOTALL)
-            shortDef = matches.group(1)+matches.group(2)
         self.name = name
-        self.shortDef = shortDef
+        self.shortDef = re.sub(r'<img.+?>',"",shortDef)
         self.url = url
         self.word = word
     
@@ -50,14 +47,65 @@ class dictionaryEntry:
             return self.shortDef
         else:
             try:
+                idNum = self.url.split('#')[-1]
                 entryPage = requests.get(self.url).text
-                return re.search(r'personal_snippet" content="(.+?)>',entryPage).group(1)
+                reg = re.compile('<div id="' + idNum + '_".+?<div class="content-box contents_area meaning_area p10">(.+?)<!-- /contents -->',re.DOTALL)
+                return cleanDefinition(re.search(reg,entryPage).group(1))
             except requests.exceptions.ConnectionError:
                 return self.shortDef
 
     def __str__(self):
         return self.word+ ": " + self.shortDef
 
+def cleanDefinition(dirty):
+    dirtyLines = re.findall(r'<p class="text">(.+?)</p>',dirty,re.DOTALL)
+    answer = ""
+    for line in dirtyLines:
+        answer += re.sub(r'<.*?>|&thinsp;|&#x32..;',"",line) + "\n"
+    return answer
+
+# adds the selected dictionary entry and closes dialog
+def buttonPressed(entry,window):
+    global finalEntries
+    finalEntries.append(entry)
+    window.close()
+
+def getBoldWords(html):
+    return re.findall(r'<b>(.+?)</b>',html)
+    
+# searches for the passed word, returning the html of the page
+def getSearchPage(word):
+    searchPage = requests.get(urlEncode(word)).text
+    if "一致する情報は見つかりませんでした" in searchPage:
+        raise ValueError("goo辞書で一致する情報は見つかりませんでした")
+    return searchPage
+
+# Returns an array containing dictionaryEntry objects corresponding to the word passed as a parameter
+def parseSearch(word): 
+    try:
+        searchPage = getSearchPage(word)
+    except ValueError:
+        return [dictionaryEntry.failedSearchEntry(word)]
+    except requests.exceptions.ConnectionError:
+        return [dictionaryEntry.connectionErrorEntry()]
+
+    try:
+        resultsString = re.search(r'<ul class="content_list idiom lsize">(.+?)</div>', searchPage, re.DOTALL).group(1)
+    except AttributeError:
+        return [dictionaryEntry.fromEntryPage(word, searchPage)]
+
+    entries = []
+    for result in re.findall(r'<a href=.+?</a>', resultsString,re.DOTALL):
+        entries.append(dictionaryEntry.fromSearchPage(word, result))
+    return entries
+
+# Returns the encoding of the word as used in goo辞書's url
+def urlEncode(word):
+    codedWord = str(word.encode('utf-8'))[2:-1].upper()
+    finalized = ""
+    for i in range(2, len(codedWord)-1 ,4):
+        finalized = finalized + "%" + codedWord[i:i+2]
+    return "https://dictionary.goo.ne.jp/srch/jn/" + finalized + "/m1u/"
 
 def theMagic(flag, n, fidx):
     global finalEntries, first, config
@@ -111,7 +159,7 @@ def theMagic(flag, n, fidx):
                         # puts the output into the note and saves
                         output = ""
                         for entry in finalEntries:
-                            output += "<div><b>"+entry.name+":</b> " + entry.getFullDef() + "</div>"
+                            output += "<div><b>"+entry.word+":</b> " + entry.getFullDef() + "</div>"
                         n[dst] = output
                         n.flush()
                         aw.editor.loadNote(focusTo=fidx+1)
@@ -121,52 +169,6 @@ def theMagic(flag, n, fidx):
                         return False
             break
     return flag
-
-# adds the selected dictionary entry and closes dialog
-def buttonPressed(entry,window):
-    global finalEntries
-    finalEntries.append(entry)
-    window.close()
-
-
-def getBoldWords(html):
-    return re.findall(r'<b>(.+?)</b>',html)
-    
-# searches for the passed word, returning the html of the page
-def getSearchPage(word):
-    searchPage = requests.get("https://dictionary.goo.ne.jp/srch/jn/" + urlEncode(word) + "/m1u/").text
-    if "一致する情報は見つかりませんでした" in searchPage:
-        searchPage = requests.get("https://dictionary.goo.ne.jp/srch/jn/" + urlEncode(word) + "/m1u/").text
-        if "一致する情報は見つかりませんでした" in searchPage:
-            raise ValueError("goo辞書で一致する情報は見つかりませんでした")
-    return searchPage
-
-# Returns an array containing dictionaryEntry objects corresponding to the word passed as a parameter
-def parseSearch(word): 
-    try:
-        searchPage = getSearchPage(word)
-    except ValueError:
-        return [dictionaryEntry.failedSearchEntry(word)]
-    except requests.exceptions.ConnectionError:
-        return [dictionaryEntry.connectionErrorEntry()]
-
-    try:
-        resultsString = re.search(r'<ul class="content_list idiom lsize">(.+?)</div>', searchPage, re.DOTALL).group(1)
-    except AttributeError:
-        return [dictionaryEntry.fromEntryPage(word, searchPage)]
-
-    entries = []
-    for result in re.findall(r'<a href=.+?</a>', resultsString,re.DOTALL):
-        entries.append(dictionaryEntry.fromSearchPage(word, result))
-    return entries
-
-# Returns the encoding of the word as used in goo辞書's url
-def urlEncode(word):
-    codedWord = str(word.encode('utf-8'))[2:-1].upper()
-    finalized = ""
-    for i in range(2, len(codedWord)-1 ,4):
-        finalized = finalized + "%" + codedWord[i:i+2]
-    return finalized
 
 # tells anki to call theMagic when focus is lost (you move out of a text field or something)
 addHook('editFocusLost', theMagic)
