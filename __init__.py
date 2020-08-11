fb6efe8a0a5d853208b0f34982f8a897341ceb66
Jackson Kjar <jkjar6@gatech.edu>
2020-08-10 23:46:24 -0400

    support for 2.1.30
    fixed a bug related to flushing note
    cleaned up code

--- __init__.py
+++ __init__.py
@@ -21,68 +21,92 @@ def buttonPressed(entry,window):
 def getBoldWords(html):
     return re.findall(r'<b>(.+?)</b>',html)
 
+def getNoteType(name):
+    global config
+    for notetype in config['notetypes']:
+        if(name == notetype['name']): 
+            return notetype
+    return None
+
+def correctOrder(flag, n, fidx):
+    global first
+    first = False
+    remHook('editFocusLost', theMagic)
+    runHook('editFocusLost',flag,n,fidx)
+    addHook('editFocusLost', theMagic)
+
+def getActiveWindow(note):
+    for widget in mw.app.allWidgets():
+        # finds the editor in use
+        if isinstance(widget, editor.EditorWebView) and widget.editor.note is note:
+            return widget
+    return None
+
+def getDefinitionChoiceDialog(aw, entries):
+    d = QDialog(aw)
+    grid = QGridLayout()
+
+    # adds found definitions to dialog window
+    for x in range(len(entries)):
+        button = QPushButton(entries[x].word)
+        button.clicked.connect(partial(buttonPressed, entries[x],d))
+        label = QLabel()
+        label.setText(entries[x].shortDef)
+        label.setWordWrap(True)
+        grid.addWidget(button,x,0)
+        grid.addWidget(label,x,1,1,5)
+    d.setLayout(grid)
+    return d
+
 def theMagic(flag, n, fidx):
     global finalEntries, first, config
     fields = mw.col.models.fieldNames(n.model())
-    for notetype in config["notetypes"]:
-        if n.model()['name'] == notetype["name"]:
-            if fields[fidx] == notetype["src"]:
-                src = fields[fidx]
-                dst = notetype["dst"]
-                try:
-                    n[dst] == n[src]
-                except:
-                    raise ValueError("The dst and src fields in config don't match those on the card")
-                # conditions to confirm functionality should be run
-                if n[dst] == "" and n[src] != "":
-                    aw = None
-                    for widget in mw.app.allWidgets():
-                        # finds the editor in use
-                        if isinstance(widget, editor.EditorWebView) and widget.editor.note is n:
-                            aw = widget
-                            break
-                    if aw != None:
-                        # because this add-on loads first and it reacts with the reading generator strangely, gotta put it in the back
-                        if first:
-                            first = False
-                            remHook('editFocusLost', theMagic)
-                            runHook('editFocusLost',flag,n,fidx)
-                            addHook('editFocusLost', theMagic)
-                        boldWords = getBoldWords(n[src])
-                        # runs the dialogs
-                        for word in boldWords:
-                            entries = definitionGetter.parseSearch(word)
-                            if(len(entries) < 2):
-                                finalEntries.append(entries[0])
-                                continue
-                            d = QDialog(aw)
-                            grid = QGridLayout()
+    notetype = getNoteType(n.model()['name'])
+    if not notetype:
+        # not a notetype that has the add-on enabled, don't do anything
+        return flag
 
-                            # adds found definitions to dialog window
-                            for x in range(len(entries)):
-                                button = QPushButton(entries[x].word)
-                                button.clicked.connect(partial(buttonPressed, entries[x],d))
-                                label = QLabel()
-                                label.setText(entries[x].shortDef)
-                                label.setWordWrap(True)
-                                grid.addWidget(button,x,0)
-                                grid.addWidget(label,x,1,1,5)
-                            d.setLayout(grid)
-                            d.exec_()
+    if fields[fidx] != notetype["src"]:
+        # not the src field that has been edited, don't do anything
+        return flag
+    src = fields[fidx]
+    dst = notetype["dst"]
 
-                        # puts the output into the note and saves
-                        output = ""
-                        for entry in finalEntries:
-                            output += "<div><b>"+entry.word+":</b> " + entry.getFullDef() + "</div>"
-                        n[dst] = output
-                        n.flush()
-                        aw.editor.loadNote(focusTo=fidx+1)
-                        finalEntries = []
+    if not dst:
+        raise ValueError("The dst and src fields in config don't match those on the card")
+    
+    if n[dst]:
+        # dst isn't empty, to avoid overwriting data, don't do anything
+        return flag
 
-                        # prevents focus from advancing and messing the edits up
-                        return False
-            break
-    return flag
+    aw = getActiveWindow(n)
+    if not aw:
+        return flag
+    
+    # because this add-on loads first and it reacts with the reading generator strangely, gotta put it in the back
+    if first:
+        correctOrder(flag, n, fidx)
+    
+    boldWords = getBoldWords(n[src])
+    # runs the dialogs
+    for word in boldWords:
+        entries = definitionGetter.parseSearch(word)
+        if(len(entries) == 1):
+            finalEntries.append(entries[0])
+        else:
+            getDefinitionChoiceDialog(aw, entries).exec_()
+
+    # puts the output into the note and saves
+    output = ""
+    for entry in finalEntries:
+        output += "<div><b>"+entry.word+":</b> " + entry.getFullDef() + "</div>"
+    n[dst] = output
+    n.to_backend_note()
+    aw.editor.loadNote(focusTo=fidx+1)
+    finalEntries = []
+
+    # prevents focus from advancing and messing the edits up
+    return False
 
 # tells anki to call theMagic when focus is lost (you move out of a text field or something)
 addHook('editFocusLost', theMagic)
